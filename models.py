f69dafe702b811d9611d27955e18cc05476cc2b3
quatpv <pvqu47@gmail.com>
2022-01-04 17:08:50 +0700

    Update init code

---
+++ models.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+from torch_geometric.nn import GCNConv
+from torchvision.models import efficientnet_b0
+from efficientnet_pytorch import EfficientNet
+from torch_geometric.data import Data
+
+from layers import GCN, HGPSLPool
+
+
+class Model(torch.nn.Module):
+    def __init__(self, args):
+        super(Model, self).__init__()
+        self.args = args
+        self.num_nodes = 16
+        self.num_features = args.num_features
+        self.nhid = args.nhid
+        self.num_classes = args.num_classes
+        self.pooling_ratio = args.pooling_ratio
+        self.dropout_ratio = args.dropout_ratio
+        self.sample = args.sample_neighbor
+        self.sparse = args.sparse_attention
+        self.sl = args.structure_learning
+        self.lamb = args.lamb
+        self.fc_channels = 1280*7*7
+
+        self.cnn = EfficientNet.from_pretrained('efficientnet-b0')
+        self.conv1 = GCNConv(self.num_features, self.nhid)
+        self.conv2 = GCN(self.nhid, self.nhid)
+        self.conv3 = GCN(self.nhid, self.nhid)
+
+        self.pool1 = HGPSLPool(self.nhid, self.pooling_ratio, self.sample, self.sparse, self.sl, self.lamb)
+        self.pool2 = HGPSLPool(self.nhid, self.pooling_ratio, self.sample, self.sparse, self.sl, self.lamb)
+
+        self.lin0 = torch.nn.Linear(self.fc_channels, self.num_features)
+        self.lin1 = torch.nn.Linear(self.nhid * 2, self.nhid)
+        self.lin2 = torch.nn.Linear(self.nhid, self.nhid // 2)
+        self.lin3 = torch.nn.Linear(self.nhid // 2, self.num_classes)
+
+    def build_graph(self, data):
+        source      = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+        destination = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+        edge_index = torch.tensor([source, destination], dtype=torch.long)
+        x = data
+        batch = torch.tensor([[i]*self.num_nodes for i in range(self.args.batch_size)])
+        batch = batch.view(-1)
+        return x, edge_index, batch
+
+    def forward(self, data):
+        # extract feature from data
+        data = data.permute(0, 2, 1, 3, 4)
+        data = data.flatten(start_dim=0, end_dim=1)
+        data = self.cnn.extract_features(data)
+        data = data.view(data.size()[0], -1)
+        data = self.lin0(data)
+
+        # convert to geometric Data structure
+        x, edge_index, batch = self.build_graph(data)
+        # print("x: ", x.shape)
+        # print("edge_index: ", edge_index.shape)
+        # print("batch: ", batch.shape)
+        edge_attr = None
+
+        x = F.relu(self.conv1(x, edge_index, edge_attr))
+        x, edge_index, edge_attr, batch = self.pool1(x, edge_index, edge_attr, batch)
+        x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv2(x, edge_index, edge_attr))
+        x, edge_index, edge_attr, batch = self.pool2(x, edge_index, edge_attr, batch)
+        x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(self.conv3(x, edge_index, edge_attr))
+        x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
+
+        x = F.relu(x1) + F.relu(x2) + F.relu(x3)
+
+        x = F.relu(self.lin1(x))
+        x = F.dropout(x, p=self.dropout_ratio, training=self.training)
+        x = F.relu(self.lin2(x))
+        x = F.dropout(x, p=self.dropout_ratio, training=self.training)
+        x = F.log_softmax(self.lin3(x), dim=-1)
+
+        return x
---
+++ train.py
@@ -0,0 +1,149 @@
+import argparse
+import glob
+import os
+import time
+
+import torch
+import torch.nn.functional as F
+from models import Model
+from torch.utils.data import DataLoader
+from dataset.loader import get_training_set, get_validation_set, get_test_set
+# from torch_geometric.loader import DataLoader
+# from torch_geometric.datasets import TUDataset
+from pprint import pprint
+
+parser = argparse.ArgumentParser()
+
+# For GNN
+parser.add_argument('--seed', type=int, default=7497, help='random seed')
+parser.add_argument('--batch_size', type=int, default=2, help='batch size')
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+parser.add_argument('--weight_decay', type=float, default=0.001, help='weight decay')
+parser.add_argument('--nhid', type=int, default=128, help='hidden size')
+parser.add_argument('--sample_neighbor', type=bool, default=True, help='whether sample neighbors')
+parser.add_argument('--sparse_attention', type=bool, default=True, help='whether use sparse attention')
+parser.add_argument('--structure_learning', type=bool, default=True, help='whether perform structure learning')
+parser.add_argument('--pooling_ratio', type=float, default=0.5, help='pooling ratio')
+parser.add_argument('--dropout_ratio', type=float, default=0.0, help='dropout ratio')
+parser.add_argument('--lamb', type=float, default=1.0, help='trade-off parameter')
+parser.add_argument('--dataset', type=str, default='ucf101', help='dataset name')
+parser.add_argument('--device', type=str, default='cpu', help='specify cuda devices')
+parser.add_argument('--epochs', type=int, default=2, help='maximum number of epochs')
+parser.add_argument('--patience', type=int, default=100, help='patience for early stopping')
+
+parser.add_argument('--num_classes', type=int, default=101, help='number of classes')
+parser.add_argument('--num_features', type=int, default=1024, help='number of features')
+
+# For video streaming
+parser.add_argument('--video_path', type=str, default='dataraw/UCF-101/video', help='path to video file')
+parser.add_argument('--dataset_file', type=str, default='dataraw/UCF-101/ucf101_01.json', help='train/val split')
+parser.add_argument('--sample_duration', type=int, default=64, help='temporal duration of inputs')
+parser.add_argument('--stride_size', type=int, default=4, help='temporal stride of inputs')
+parser.add_argument('--sample_size', type=int, default=224, help='height and width of inputs')
+parser.add_argument('--n_threads', type=int, default=4, help='Number of threads for multi-thread loading')
+
+
+args = parser.parse_args()
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(args.seed)
+
+
+training_data = get_training_set(args)
+train_loader = DataLoader(training_data,
+                          batch_size=args.batch_size,
+                          shuffle=True,
+                          pin_memory=True,
+                          drop_last=True, 
+                          sampler=None)
+
+validation_data = get_validation_set(args)
+val_loader = DataLoader(validation_data,
+                        batch_size=args.batch_size,
+                        shuffle=False,
+                        pin_memory=True)
+
+model = Model(args).to(args.device)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+
+def train():
+    min_loss = 1e10
+    patience_cnt = 0
+    val_loss_values = []
+    best_epoch = 0
+
+    t = time.time()
+    model.train()
+    for epoch in range(args.epochs):
+        loss_train = 0.0
+        correct = 0
+        for i, (inputs, targets, index) in enumerate(train_loader):
+            optimizer.zero_grad()
+            inputs = inputs.to(args.device)
+            targets = targets.to(args.device)
+
+            out = model(inputs)
+            loss = F.nll_loss(out, targets)
+            loss.backward()
+            optimizer.step()
+            loss_train += loss.item()
+            pred = out.max(dim=1)[1]
+            print("pred: ", pred)
+            correct += pred.eq(targets).sum().item()
+            print("correct: ", correct)
+        acc_train = correct / len(train_loader.dataset)
+        acc_val, loss_val = compute_test(val_loader)
+        print('Epoch: {:04d}'.format(epoch + 1), 'loss_train: {:.6f}'.format(loss_train),
+              'acc_train: {:.6f}'.format(acc_train), 'loss_val: {:.6f}'.format(loss_val),
+              'acc_val: {:.6f}'.format(acc_val), 'time: {:.6f}s'.format(time.time() - t))
+
+        val_loss_values.append(loss_val)
+        torch.save(model.state_dict(), 'weights/{}.pth'.format(epoch))
+        if val_loss_values[-1] < min_loss:
+            min_loss = val_loss_values[-1]
+            best_epoch = epoch
+            patience_cnt = 0
+        else:
+            patience_cnt += 1
+
+        if patience_cnt == args.patience:
+            break
+
+        files = glob.glob('weights/*.pth')
+        for f in files:
+            epoch_nb = int(f[8:].split('.')[0])
+            if epoch_nb < best_epoch:
+                os.remove(f)
+
+    files = glob.glob('weights/*.pth')
+    for f in files:
+        epoch_nb = int(f[8:].split('.')[0])
+        if epoch_nb > best_epoch:
+            os.remove(f)
+    print('Optimization Finished! Total time elapsed: {:.6f}'.format(time.time() - t))
+
+    return best_epoch
+
+
+def compute_test(loader):
+    model.eval()
+    correct = 0.0
+    loss_test = 0.0
+    for data in loader:
+        data = data.to(args.device)
+        out = model(data)
+        pred = out.max(dim=1)[1]
+        correct += pred.eq(data.y).sum().item()
+        loss_test += F.nll_loss(out, data.y).item()
+    return correct / len(loader.dataset), loss_test
+
+
+if __name__ == '__main__':
+    # Model training
+    best_model = train()
+
+    # Restore best model for test set
+    # model.load_state_dict(torch.load('weights/{}.pth'.format(best_model)))
+    # test_acc, test_loss = compute_test(test_loader)
+    # print('Test set results, loss = {:.6f}, accuracy = {:.6f}'.format(test_loss, test_acc))
+
